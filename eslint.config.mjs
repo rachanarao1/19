f67c8c5ff5161e406ee74cd2bce5adca4c8b0845
Rachana <rachana.rao@kyndryl.com>
2025-08-19 16:41:07 +0530

    first

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,4 @@
 import js from '@eslint/js';
-<<<<<<< HEAD
-
-export default [
-  {
-    ...js.configs.recommended,
-    files: ['**/*.js'],
-  }
-];
-
-=======
 
 export default [
   {
@@ -16,13 +6,12 @@ export default [
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
-      globals: js.configs.recommended.languageOptions.globals
+      globals: js.configs.recommended.languageOptions.globals,
     },
     rules: {
       ...js.configs.recommended.rules,
-      semi: ['error', 'always'],
-      quotes: ['error', 'single']
-    }
-  }
+      semi: ['error', 'always'],        // enforce semicolons
+      quotes: ['error', 'single'],      // enforce single quotes
+    },
+  },
 ];
->>>>>>> 8d9c138bbf25dfdc51b4e25272c854cbdf7a5ac6
